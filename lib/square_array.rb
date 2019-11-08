f63bb41d296959fbccac9d8b4c0857eb7be7d5a6
kowalix007 <kowalix007@gmail.com>
2019-11-08 15:44:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
+  def square_array(array)
   # your code here
-end
+  counter = 0
+ 
+  while counter < array.length do
+    new_list.push(array[counter]**2)
+   counter += 1 
+  end
+  new_list
+
+  end
